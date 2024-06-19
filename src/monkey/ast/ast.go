186195272b6101c67fbe15033234562b17f95306
Jack Wiebe <jack.b.wiebe@gmail.com>
2024-06-18 21:22:41 -0400

    feat: added ast and parser

---
+++ src/monkey/ast/ast.go
@@ -0,0 +1,50 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name *Indentifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode(){}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Indentifier struct{
+	Token token.Token
+	Value string
+}
+
+func (i *Indentifier) expressionNode(){}
+func (i *Indentifier) TokenLiteral() string {
+	return i.Token.Literal
+}
